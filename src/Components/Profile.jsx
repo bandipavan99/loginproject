73001905d960e48aa0adb2abb4f8b20511de11ee
loginproject <bandipavankumar2005@gmail.com>
2025-05-11 07:24:24 +0530

    updated

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -6,8 +6,8 @@ function Profile({ user }) {
   return (
     <div>
       <h2>User Profile</h2>
-      <p><strong>Name:</strong> {user?.name || "Srinu"}</p>
-      <p><strong>Email:</strong> {user?.email || "srininivasreddybranham2004@gmail.com"}</p>
+      <p><strong>Name:</strong> {user?.name || "pavan"}</p>
+      <p><strong>Email:</strong> {user?.email || "pavankumar2005@gmail.com"}</p>
       <button onClick={() => navigate("/dashboard")}>Return to Home</button>
     </div>
   );
